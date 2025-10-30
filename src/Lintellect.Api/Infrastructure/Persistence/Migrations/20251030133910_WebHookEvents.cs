f7bf4a74ac8ab10bf98b3edbad06a157e69e244e
DawnDevelop <denizjoecks@yahoo.de>
2025-10-30 14:39:15 +0100

    fixed migration

---
+++ src/Lintellect.Api/Infrastructure/Persistence/Migrations/20251030133910_WebHookEvents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lintellect.Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebHookEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebhookEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventType = table.Column<int>(type: "integer", nullable: false),
+                    Provider = table.Column<int>(type: "integer", nullable: false),
+                    EventPayload = table.Column<JsonDocument>(type: "jsonb", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ReceivedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ProcessedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhookEvents", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebhookEvents");
+        }
+    }
+}
